1f144f1be0827de2260029103b29cc44e787a1ba
Kobi-Blade <local_host@zoho.eu>
2025-01-13 10:23:46 +0000

    Refactored ModCollectionIdentity.cs

--- Penumbra/Collections/ModCollectionIdentity.cs
+++ Penumbra/Collections/ModCollectionIdentity.cs
@@ -3,37 +3,42 @@ using Penumbra.Collections.Manager;
 
 namespace Penumbra.Collections;
 
-public struct ModCollectionIdentity(Guid id, LocalCollectionId localId)
+public struct ModCollectionIdentity
 {
     public const string DefaultCollectionName = "Default";
-    public const string EmptyCollectionName   = "None";
+    public const string EmptyCollectionName = "None";
 
     public static readonly ModCollectionIdentity Empty = new(Guid.Empty, LocalCollectionId.Zero, EmptyCollectionName, 0);
 
-    public string            Name    { get; set; }
-    public Guid              Id      { get; } = id;
-    public LocalCollectionId LocalId { get; } = localId;
+    public string Name { get; set; } = string.Empty;
+    public Guid Id { get; }
+    public LocalCollectionId LocalId { get; }
 
     /// <summary> The index of the collection is set and kept up-to-date by the CollectionManager. </summary>
     public int Index { get; internal set; }
 
-    public string Identifier
-        => Id.ToString();
+    public string Identifier => Id.ToString();
 
-    public string ShortIdentifier
-        => Id.ShortGuid();
+    public string ShortIdentifier => Id.ShortGuid();
 
     /// <summary> Get the short identifier of a collection unless it is a well-known collection name. </summary>
-    public string AnonymizedName
-        => Id == Guid.Empty ? EmptyCollectionName : Name == DefaultCollectionName ? Name : ShortIdentifier;
+    public string AnonymizedName => Id == Guid.Empty ? EmptyCollectionName : Name == DefaultCollectionName ? Name : ShortIdentifier;
 
-    public override string ToString()
-        => Name.Length > 0 ? Name : ShortIdentifier;
+    public override string ToString() => Name.Length > 0 ? Name : ShortIdentifier;
+
+    public ModCollectionIdentity(Guid id, LocalCollectionId localId)
+    {
+        Id = id;
+        LocalId = localId;
+        Name = string.Empty;
+        Index = 0;
+    }
 
     public ModCollectionIdentity(Guid id, LocalCollectionId localId, string name, int index)
-        : this(id, localId)
     {
-        Name  = name;
+        Id = id;
+        LocalId = localId;
+        Name = name;
         Index = index;
     }
 
